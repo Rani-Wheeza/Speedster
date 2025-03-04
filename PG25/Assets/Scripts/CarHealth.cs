cfbbd0604abd0a405f8f81a14a008485d3177dce
Rani-Wheeza <151739896+Rani-Wheeza@users.noreply.github.com>
2025-03-04 09:55:09 +0000

    Signed-off-by: Rani-Wheeza <151739896+Rani-Wheeza@users.noreply.github.com>

---
+++ PG25/Assets/Scripts/CarHealth.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth;
+    public Slider healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void UpdateHealthBar()
+    {
+        if(healthBar != null)
+        {
+            healthBar.value = currentHealth / maxHealth;
+        }
+    }
+}
+
--- PG25/Assets/Scripts/Collions Script.cs
+++ PG25/Assets/Scripts/Collions Script.cs
@@ -41,68 +41,10 @@ public class CollionsScript : MonoBehaviour
         }
     }
 
-    /*private void OnCollisionEnter(Collision collision)
-    {
-
-        //Check if the car collides with the wall by tag (optional)
-        if (collision.gameObject.CompareTag("Wall"))
-        {
-
-            //Play crash sound
-            if (crashSound != null) 
-            {
-                audioSource.PlayOneShot(crashSound);
-            
-            }
-
-            //Apply a force to the car to simulate a crash (you can adjust crashForce)
-            ApplyCrashForce(collision);
-
-            // You could also slow down the car or do other things here
-             carRigidbody.velocity = Vector3.zero;  // For example, stop the car
-        }
-    }
-
-    private void ApplyCrashForce(Collision collision)
-    {
-        // Calculate the impact force of the collision
-        Vector3 collisionNomarl = collision.contacts[0].normal;
-        carRigidbody.AddForce(-collisionNomarl *  crashForce, ForceMode.Impulse);
-
-        // You can also add a small effect like reducing speed after crash
-         carRigidbody.velocity *= 0.5f; // Slow down the car after crash
-    }
-
     
-     public ParticleSystem crashParticles;  // Drag and drop particle system in the inspector
-
-private void OnCollisionEnter(Collision collision)
-{
-    if (collision.gameObject.CompareTag("Wall"))
-    {
-        // Play crash sound
-        if (crashSound != null)
-        {
-            audioSource.PlayOneShot(crashSound);
-        }
-
-        // Trigger particles
-        if (crashParticles != null)
-        {
-            crashParticles.transform.position = collision.contacts[0].point;
-            crashParticles.Play();
-        }
-
-        // Apply crash force
-        ApplyCrashForce(collision);
-    }*/
+    
 }
 
      
 
-    // Update is called once per frame
-    /*void Update()
-    {
-        
-    }
-}*/
+   
